99dcfaab0f7172973f3d6b8e231ae339847e997a
unilock <unilock@fennet.rentals>
2024-02-25 21:59:39 -0500

    Fix dropped blocks being duplicated on Forge (via Sinytra Connector)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/dev/draylar/magna/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/dev/draylar/magna/mixin/ServerPlayerInteractionManagerMixin.java
@@ -2,11 +2,14 @@ package dev.draylar.magna.mixin;
 
 import dev.draylar.magna.impl.MagnaPlayerInteractionManagerExtension;
 import dev.draylar.magna.api.MagnaTool;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.OperatorBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,28 +21,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = ServerPlayerInteractionManager.class, priority = 1001)
 public class ServerPlayerInteractionManagerMixin implements MagnaPlayerInteractionManagerExtension {
 
+    @Shadow public GameMode gameMode;
     @Shadow public ServerPlayerEntity player;
     @Shadow public ServerWorld world;
     @Unique private boolean magna_isMining = false;
 
     @Inject(
             method = "tryBreakBlock",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/Block;onBreak(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/entity/player/PlayerEntity;)V"
-            ),
+            at = @At("HEAD"),
             cancellable = true
     )
     private void tryBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        ItemStack heldStack = player.getMainHandStack();
+        // The original checks from tryBlockBreak before onBreak is called
+        BlockState blockState = this.world.getBlockState(pos);
+        if (this.player.getMainHandStack().getItem().canMine(blockState, this.world, pos, this.player)
+                && !(blockState.getBlock() instanceof OperatorBlock && !this.player.isCreativeLevelTwoOp())
+                && !this.player.isBlockBreakingRestricted(this.world, pos, this.gameMode)) {
+            ItemStack heldStack = player.getMainHandStack();
 
-        if (heldStack.getItem() instanceof MagnaTool) {
-            // This is to avoid recursion, but the goal is to make sure every block it doesn't override cancelled block breaks using Fabric's callbacks. This was made to support claim mods.
-            boolean v = magna_isMining || ((MagnaTool) heldStack.getItem()).attemptBreak(world, pos, player, ((MagnaTool) heldStack.getItem()).getRadius(heldStack), ((MagnaTool) heldStack.getItem()).getProcessor(world, player, pos, heldStack));
+            if (heldStack.getItem() instanceof MagnaTool) {
+                // This is to avoid recursion, but the goal is to make sure every block it doesn't override cancelled block breaks using Fabric's callbacks. This was made to support claim mods.
+                boolean v = magna_isMining || ((MagnaTool) heldStack.getItem()).attemptBreak(world, pos, player, ((MagnaTool) heldStack.getItem()).getRadius(heldStack), ((MagnaTool) heldStack.getItem()).getProcessor(world, player, pos, heldStack));
 
-            // only cancel if the break was successful (false is returned if the player is sneaking)
-            if(v) {
-                cir.setReturnValue(true);
+                // only cancel if the break was successful (false is returned if the player is sneaking)
+                if(v) {
+                    cir.setReturnValue(true);
+                }
             }
         }
     }
